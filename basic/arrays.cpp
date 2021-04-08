c1217459faeb2c72c77c7f7644b1940d0588bf3e
RohanKittu <rohanvarmasmit@gmail.com>
2021-04-08 23:40:00 +0530

    new

--- basic/arrays.cpp
+++ basic/arrays.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 //library for knowing the type of the variable
+//ddd
 #include <typeinfo>
 using namespace std;
 /*
@@ -13,27 +14,27 @@ An array is an fixed size memory.we can not define the size dynamically.
 int main()
 {
     //declaring an single dimension array
-    int first_array[5] = {0,1,2,3,4};
+    int first_array[5] = {0, 1, 2, 3, 4};
 
     //printing an array
-    cout<<"\n array address of first element in the array :- "<<first_array;
+    cout << "\n array address of first element in the array :- " << first_array;
 
     //array indexing
-    cout<<"\n 1st element :- "<<first_array[0];
-    cout<<"\n 2nd element :- "<<first_array[1];
-    cout<<"\n 3rd element :- "<<first_array[2];
-    cout<<"\n 4th element :- "<<first_array[3];
-    cout<<"\n 5th element :- "<<first_array[4];
+    cout << "\n 1st element :- " << first_array[0];
+    cout << "\n 2nd element :- " << first_array[1];
+    cout << "\n 3rd element :- " << first_array[2];
+    cout << "\n 4th element :- " << first_array[3];
+    cout << "\n 5th element :- " << first_array[4];
 
     //assiging  a value to an array element
     first_array[0] = 10;
-    cout<<"\n 1st element after assignmet :- "<<first_array[0]<<"\n";
+    cout << "\n 1st element after assignmet :- " << first_array[0] << "\n";
 
     //creating a charactor array
-    char name[] = {'r','o','h','a','n'};
+    char name[] = {'r', 'o', 'h', 'a', 'n'};
 
     // declaring multi dim array
-    int m[2][2] = {1,2,3,4};
+    int m[2][2] = {1, 2, 3, 4};
 
     return 0;
 }
